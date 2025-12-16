0847c5518fed13ce899230e8fbb5d3be00b59a1f
Степан <stepancat61@gmail.com>
2025-12-16 08:08:22 +0300

    add first scr page - main

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import styles from "./Header.module.css";
 import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const scrollHandler = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    // очистка при размонтировании
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isScrolled ? styles.scrolled : ""}`}>
       <div className={styles.brand}>
         <img src="nevas_logo.svg" alt="logotype" />
         <label className={styles.gradientText}>NEVAS</label>
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,3 +1,26 @@
+import Button from "../../components/Button/Button";
+import styles from "./Main.module.css";
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div className="page">
+      <div>
+        <section className={styles.contentBlock}>
+          <h1 className={styles.mainText}>Фокусировка</h1>
+          <h2 className={styles.dwnText}>
+            первый шаг к <p style={{ color: "#3CC58B" }}>прогрессу</p>
+          </h2>
+          <p className={styles.bigTextSection}>
+            Сфокусируйся на главном, твое внимание постоянно уходит в сторону.
+            Неозможно сбиться с пути, если каждый твой шаг осознан.
+          </p>
+          <div className={styles.btnSec}>
+            <Button>Посмотреть пространство</Button>
+          </div>
+        </section>
+      </div>
+      <section className={styles.imageSection}>
+        <img src="mainLine.svg" alt="" className={styles.mainImage} />
+      </section>
+    </div>
+  );
 }
